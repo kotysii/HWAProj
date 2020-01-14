1d365dcdd77b49deb6f89f3772c13412147cb5c1
Kotys Two <kotystwo@gmmail.com>
2020-01-14 11:23:03 +0000

    trying again: deleted some obtuse file; added a little sin cos resolution maker;

--- HW/HoughAccelerator/APractice.v
+++ HW/HoughAccelerator/APractice.v
@@ -1,28 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01.12.2019 18:44:41
-// Design Name: 
-// Module Name: APractice
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module APractice(input[3:0] in, output[3:0] out);
-    out[2] != in[2]
-    out[1] != in[1]
-    out[0] != in[0]
+
+assign 
     
 endmodule
